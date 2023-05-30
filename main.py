8352d5b17118be35ed4dc7a3125b2b4242a2330f
Ben-Benston <73870603+Ben-Benston@users.noreply.github.com>
2023-05-30 09:44:19 +0000

    Minor Changes

--- main.py
+++ main.py
@@ -6,8 +6,6 @@ from keep_alive import keep_alive
 from discord import Embed
 
 intents = discord.Intents.default()
-intents.typing = False
-intents.presences = False
 intents.message_content = True
 
 bot = commands.Bot(command_prefix='!', intents=intents)
